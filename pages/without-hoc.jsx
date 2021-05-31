553871eb763bbc6a755c13a42c9af35058c501b5
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-31 16:51:08 +0700

    feat: add without-hoc to show difference without invisible

---
+++ pages/without-hoc.jsx
@@ -0,0 +1,45 @@
+import Seo from '@/components/Seo';
+import CustomLink from '@/components/CustomLink';
+import { Tween } from 'react-gsap';
+
+export default function WithoutHoc() {
+  return (
+    <>
+      <Seo />
+
+      <main>
+        <section className='bg-dark'>
+          <div className='flex flex-col items-center justify-center min-h-screen text-white layout'>
+            <Tween
+              from={{ y: '50px', opacity: 0 }}
+              to={{ y: 0, opacity: 1 }}
+              duration={1}
+              ease='power2.out'
+              stagger={1}
+            >
+              <h1>Learning React GSAP</h1>
+              <p className='mt-2 text-gray-400'>
+                Check out{' '}
+                <CustomLink href='https://github.com/theodorusclarence/learn-react-gsap'>
+                  my repository
+                </CustomLink>{' '}
+                for the code
+              </p>
+              <h2>
+                <CustomLink className='mt-8' href='/fade'>
+                  Start to the fade page
+                </CustomLink>
+              </h2>
+            </Tween>
+            <footer className='absolute text-gray-500 bottom-2'>
+              © {new Date().getFullYear()} By{' '}
+              <CustomLink href='https://theodorusclarence.com'>
+                Theodorus Clarence
+              </CustomLink>
+            </footer>
+          </div>
+        </section>
+      </main>
+    </>
+  );
+}
