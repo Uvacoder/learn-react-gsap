346221c1c8243b95d887e59ef198ee70fb5c5dd1
Theodorus Clarence <theodorusclarence@gmail.com>
2021-05-31 12:57:49 +0700

    feat: add staggering grid

--- pages/stagger.jsx
+++ pages/stagger.jsx
@@ -18,9 +18,10 @@ export default function Stagger() {
               stagger={0.2}
               ease='power2.out'
             >
-              <h1>
-                <CustomLink href='/scroll-trigger'>To ScrollTrigger</CustomLink>
-              </h1>
+              <div className='flex space-x-4'>
+                <CustomLink href='/fade'>← Back</CustomLink>
+                <CustomLink href='/reveal'>To The Reveal Page →</CustomLink>
+              </div>
               <p className='text-gray-300'>I will run</p>
               <p className='text-gray-300'>individually</p>
               <div className='w-5 h-5 bg-white'></div>
@@ -47,11 +48,28 @@ export default function Stagger() {
 
               <div className='w-5 h-5 bg-white'></div>
               <div className='w-5 h-5 bg-white'></div>
-              <div className='w-5 h-5 bg-white'></div>
+
+              {/* Child Stagger */}
+              <div className='grid grid-cols-4 gap-2'>
+                <Tween
+                  from={{ scale: 0.2, opacity: 0 }}
+                  duration={1}
+                  delay={0.7}
+                  stagger={{ from: 'center', amount: 1, grid: [4, 4] }}
+                  ease='power2.out'
+                >
+                  {[...Array(16)].map((e, i) => (
+                    <div className='w-5 h-5 bg-primary-400' key={i}></div>
+                  ))}
+                </Tween>
+              </div>
             </Tween>
 
-            <footer className='absolute text-gray-300 bottom-2'>
-              © {new Date().getFullYear()}
+            <footer className='absolute text-gray-500 bottom-2'>
+              © {new Date().getFullYear()} By{' '}
+              <CustomLink href='https://theodorusclarence.com'>
+                Theodorus Clarence
+              </CustomLink>
             </footer>
           </div>
         </section>
